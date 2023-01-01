2b48b0993b3d3b3e415d27067ab380e9971671f5
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-01-02 00:48:19 +0300

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -57,6 +57,31 @@ public class Main {
         if (deliveryDays !=0) System.out.println("Потребуется дней: "+ deliveryDays);
     }
     public static void task5() {
+        int monthNumber = 10;
         System.out.println("Задача 5");
+        switch (monthNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Ошибка ввода данных. В году только 12 месяцев");
+        }
     }
 }
